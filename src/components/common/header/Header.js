e39755dcacb8bde22502d2d8f02da0d464d16dc4
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-02-11 22:50:03 +0100

    Added: basic styles

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from './logo/Logo';
+import './Header.css';
 
 export default () => {
   const applicationName = 'Password backpack'
-  const tagline = 'Trust only yourself';
+  const tagline = 'All passwords in one place';
   const activeClass = 'active';
   const links = [
     { label: 'Dashboard', href: '/'},
@@ -15,7 +16,7 @@ export default () => {
     <header>
       <Logo></Logo>
       <h1>{applicationName}</h1>
-      <h2>{tagline}</h2>
+      <h3>{tagline}</h3>
       <nav>
         {links.map(link => <NavLink exact to={link.href} activeClassName={activeClass} key={link.label}>{link.label}</NavLink>)}
       </nav>
