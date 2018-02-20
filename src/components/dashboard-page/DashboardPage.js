6cddcaa9fd1c97096e392637741d8b7a348d149f
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-02-20 22:37:07 +0100

    open file dialog && page height adjustment

--- src/components/dashboard-page/DashboardPage.js
+++ src/components/dashboard-page/DashboardPage.js
@@ -5,14 +5,41 @@ import Tile from '../common/tile/Tile';
 import Plus from '../../icons/plus.svg';
 import Key from '../../icons/key.svg';
 
+const electron = window.require('electron');
+const { dialog } = electron.remote;
+
 class DashboardPage extends React.Component {
   constructor(props) {
     super(props);
+
     this.navigateToCreator = this.navigateToCreator.bind(this);
+    this.showOpenFileDialog = this.showOpenFileDialog.bind(this);
+
+    this.staticTexts = new Map([
+      ['alt.create_new', 'Click to create new password file'],
+      ['desc.create_new', 'Create new password file'],
+      ['alt.decrypt', 'Click to decrypt existing file'],
+      ['desc.decrypt', 'Decrypt existing file'],
+    ]);
   }
 
   navigateToCreator() {
     this.props.history.push('/create-new');
+  }
+
+  showOpenFileDialog() {
+    dialog.showOpenDialog(
+      {  
+        properties: ['openFile'],
+        filters: [{ name: 'All Files', extensions: ['json'] }]
+      },
+      this.parseJsonFile
+    );
+  }
+
+  parseJsonFile(name) {
+    const [fileName] = name;
+    console.log(fileName);
   }
 
   render() {
@@ -20,13 +47,13 @@ class DashboardPage extends React.Component {
       <section className="dashboard-page">
         <Tile doAfterClick={this.navigateToCreator}
               imageSource={Plus} 
-              alternativeText="Click to create new password file" 
-              descriptiveText="create new password file">
+              alternativeText={this.staticTexts.get('alt.create_new')} 
+              descriptiveText={this.staticTexts.get('desc.create_new')}>
         </Tile>
-        <Tile doAfterClick={this.navigateToCreator} 
+        <Tile doAfterClick={this.showOpenFileDialog} 
               imageSource={Key} 
-              alternativeText="Click to decrypt existing file" 
-              descriptiveText="Decrypt existing file">
+              alternativeText={this.staticTexts.get('alt.decrypt')} 
+              descriptiveText={this.staticTexts.get('desc.decrypt')}>
         </Tile>
       </section>
     );
