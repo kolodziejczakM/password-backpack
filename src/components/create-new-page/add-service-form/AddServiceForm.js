7f2eb228e1fc11c08e5e8a05d8006ae1b2d5658d
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-03-01 22:28:16 +0100

    Basic add-service-form styling

--- src/components/create-new-page/add-service-form/AddServiceForm.js
+++ src/components/create-new-page/add-service-form/AddServiceForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { head } from 'ramda';
+import './AddServiceForm.css';
 import ServiceTemplatesProvider from '../../../providers/ServiceTemplatesProvider';
 
 const PASSWORD_ID = 'PASSWORD';
@@ -10,10 +11,12 @@ const PREVIEW_DIMENSION = 100;
 const staticTexts = new Map([
   ['select.login', 'Login'],
   ['select.password', 'Password'],
-  ['placeholder.password', 'Type the value'],
+  ['placeholder.password', 'Type the value of'],
+  ['placeholder.service_name', 'Type service name'],
   ['label.choose.service_template', 'Choose a service template:'],
   ['label.choose.service_name', 'Name current service:'],
   ['button.submit_label', 'Append service'],
+  ['alt.service_icon_preview', 'Service icon preview'],
 ]);
 
 const passwordTypes = [
@@ -68,12 +71,15 @@ class AddServiceForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="add-service-form">
         <fieldset>
-          <label>
-            {staticTexts.get('label.choose.service_template')}
-
-            <select value={this.state.service.templateName} onChange={this.onWholeServiceChange}>
+          <label className="add-service-form__label-container">
+            <span className="add-service-form__label">{staticTexts.get('label.choose.service_template')}</span>
+            <select
+              className="add-service-form__select"
+              value={this.state.service.templateName}
+              onChange={this.onWholeServiceChange}
+            >
               {ServiceTemplatesProvider.getServiceTemplates().map(template => (
                 <option key={template.templateName} value={template.templateName}>
                   {template.templateName}
@@ -81,43 +87,48 @@ class AddServiceForm extends React.Component {
               ))}
             </select>
           </label>
-
-          <label>
-            {staticTexts.get('label.choose.service_name')}
-
+          <label className="add-service-form__label-container">
+            <span className="add-service-form__label">{staticTexts.get('label.choose.service_name')}</span>
             <input
+              className="add-service-form__text-input"
               type="text"
               value={this.state.service.name}
-              placeholder="Type service name"
+              placeholder={staticTexts.get('placeholder.service_name')}
               onChange={this.onServiceNameChange}
             />
           </label>
-
+          <label className="add-service-form__label-container">
+            <select
+              className="add-service-form__select add-service-form__select--label"
+              value={this.state.passwordType}
+              onChange={this.onPasswordTypeChange}
+            >
+              {passwordTypes.map(passwordType => (
+                <option key={passwordType.value} value={passwordType.value}>
+                  {passwordType.label}
+                </option>
+              ))}
+            </select>
+            <input
+              className="add-service-form__text-input"
+              type="text"
+              value={this.state.service.passwordValue}
+              placeholder={staticTexts.get('placeholder.password')}
+              onChange={this.onPasswordValueChange}
+            />
+          </label>
+          <button className="add-service-form__submit-btn" type="submit">{staticTexts.get('button.submit_label')}</button>
         </fieldset>
-
-        <fieldset>
-          <select value={this.state.passwordType} onChange={this.onPasswordTypeChange}>
-            {passwordTypes.map(passwordType => (
-              <option key={passwordType.value} value={passwordType.value}>
-                {passwordType.label}
-              </option>
-            ))}
-          </select>
-
-          <input
-            type="text"
-            value={this.state.service.passwordValue}
-            placeholder={staticTexts.get('placeholder.password')}
-            onChange={this.onPasswordValueChange}
-          />
+        <fieldset className="icon-preview-section">
+          <div className="icon-preview-frame">
+            <img
+              src={this.state.service.icon}
+              alt={staticTexts.get('alt.service_icon_preview')}
+              width={PREVIEW_DIMENSION}
+              height={PREVIEW_DIMENSION}
+            />
+          </div>
         </fieldset>
-        <img
-          alt="Service icon preview"
-          width={PREVIEW_DIMENSION}
-          height={PREVIEW_DIMENSION}
-          src={this.state.service.icon}
-        />
-        <button type="submit">{staticTexts.get('button.submit_label')}</button>
       </form>
     );
   }
