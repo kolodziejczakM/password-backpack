0ba5cf209e2d45303cba4b7850c4211599940401
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-03-01 21:23:37 +0100

    ServiceTemplatesProvider

--- src/components/create-new-page/add-service-form/AddServiceForm.js
+++ src/components/create-new-page/add-service-form/AddServiceForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { head } from 'ramda';
-import CustomIcon from '../../../icons/services/custom.svg';
-import DropboxIcon from '../../../icons/services/dropbox.svg';
+import ServiceTemplatesProvider from '../../../providers/ServiceTemplatesProvider';
 
 const PASSWORD_ID = 'PASSWORD';
 const LOGIN_ID = 'LOGIN';
+const PREVIEW_DIMENSION = 100;
 
 const staticTexts = new Map([
   ['select.login', 'Login'],
@@ -16,55 +16,13 @@ const staticTexts = new Map([
   ['button.submit_label', 'Append service'],
 ]);
 
-// TODO: Move to HoC / Render props
-const createServiceTemplate = (templateName, name = templateName, icon = '', passwordValue = '') => (
-  {
-    templateName, name, icon, passwordValue,
-  }
-);
-
-// TODO: Move to HoC / Render props
-const serviceNames = [
-  'Dropbox',
-  // 'Facebook',
-  // 'Gmail',
-  // 'Google+',
-  // 'Instagram',
-  // 'Linkedin',
-  // 'Medium',
-  // 'Mega',
-  // 'Netflix',
-  // 'PayPal',
-  // 'Pinterest',
-  // 'Skype',
-  // 'SoundCloud',
-  // 'Spotify',
-  // 'Telegram',
-  // 'Trello',
-  // 'Twitter',
-  // 'Unsplash',
-  // 'WhatsApp',
-  // 'Wordpress',
-  // 'Youtube',
-];
-
-// TODO: Move to HoC / Render props
-const serviceTemplates = [
-  createServiceTemplate('Custom', '', CustomIcon),
-  ...serviceNames.map(serviceName => createServiceTemplate(serviceName, undefined, `${serviceName}Icon`)),
-];
-
-const getServicetemplateByName = templateName => (
-  serviceTemplates.find(template => template.templateName === templateName)
-);
-
 const passwordTypes = [
   { label: staticTexts.get('select.password'), value: PASSWORD_ID },
   { label: staticTexts.get('select.login'), value: LOGIN_ID },
 ];
 
 const initialState = {
-  service: head(serviceTemplates),
+  service: head(ServiceTemplatesProvider.getServiceTemplates()),
   passwordType: head(passwordTypes),
 };
 
@@ -93,7 +51,9 @@ class AddServiceForm extends React.Component {
   }
 
   onWholeServiceChange(event) {
-    this.setState({ service: getServicetemplateByName(event.target.value) });
+    this.setState({
+      service: ServiceTemplatesProvider.getServiceTemplateByName(event.target.value),
+    });
   }
 
   resetForm() {
@@ -114,7 +74,7 @@ class AddServiceForm extends React.Component {
             {staticTexts.get('label.choose.service_template')}
 
             <select value={this.state.service.templateName} onChange={this.onWholeServiceChange}>
-              {serviceTemplates.map(template => (
+              {ServiceTemplatesProvider.getServiceTemplates().map(template => (
                 <option key={template.templateName} value={template.templateName}>
                   {template.templateName}
                 </option>
@@ -151,10 +111,12 @@ class AddServiceForm extends React.Component {
             onChange={this.onPasswordValueChange}
           />
         </fieldset>
-        <div>
-          <img alt="Service icon preview" width="100" height="100" src={CustomIcon} />
-          <img alt="Service icon preview 2" width="100" height="100" src={DropboxIcon} />
-        </div>
+        <img
+          alt="Service icon preview"
+          width={PREVIEW_DIMENSION}
+          height={PREVIEW_DIMENSION}
+          src={this.state.service.icon}
+        />
         <button type="submit">{staticTexts.get('button.submit_label')}</button>
       </form>
     );
---
+++ src/providers/ServiceTemplatesProvider.js
@@ -0,0 +1,66 @@
+import CustomIcon from '../icons/services/custom.svg';
+import DropboxIcon from '../icons/services/dropbox.svg';
+import FacebookIcon from '../icons/services/facebook.svg';
+import GmailIcon from '../icons/services/gmail.svg';
+import GooglePlusIcon from '../icons/services/googleplus.svg';
+import InstagramIcon from '../icons/services/instagram.svg';
+import LinkedinIcon from '../icons/services/linkedin.svg';
+import MediumIcon from '../icons/services/medium.svg';
+import MegaIcon from '../icons/services/mega.svg';
+import NetflixIcon from '../icons/services/netflix.svg';
+import PayPalIcon from '../icons/services/paypal.svg';
+import PinterestIcon from '../icons/services/pinterest.svg';
+import SkypeIcon from '../icons/services/skype.svg';
+import SoundCloudIcon from '../icons/services/soundcloud.svg';
+import SpotifyIcon from '../icons/services/spotify.svg';
+import TelegramIcon from '../icons/services/telegram.svg';
+import TrelloIcon from '../icons/services/trello.svg';
+import TwitterIcon from '../icons/services/twitter.svg';
+import UnsplashIcon from '../icons/services/unsplash.svg';
+import WhatsAppIcon from '../icons/services/whatsapp.svg';
+import WordpressIcon from '../icons/services/wordpress.svg';
+import YoutubeIcon from '../icons/services/youtube.svg';
+
+const customService = { name: 'Custom', src: CustomIcon };
+const supportingServices = [
+  { name: 'Dropbox', src: DropboxIcon },
+  { name: 'Facebook', src: FacebookIcon },
+  { name: 'Gmail', src: GmailIcon },
+  { name: 'Google+', src: GooglePlusIcon },
+  { name: 'Instagram', src: InstagramIcon },
+  { name: 'Linkedin', src: LinkedinIcon },
+  { name: 'Medium', src: MediumIcon },
+  { name: 'Mega', src: MegaIcon },
+  { name: 'Netflix', src: NetflixIcon },
+  { name: 'PayPal', src: PayPalIcon },
+  { name: 'Pinterest', src: PinterestIcon },
+  { name: 'Skype', src: SkypeIcon },
+  { name: 'SoundCloud', src: SoundCloudIcon },
+  { name: 'Spotify', src: SpotifyIcon },
+  { name: 'Telegram', src: TelegramIcon },
+  { name: 'Trello', src: TrelloIcon },
+  { name: 'Twitter', src: TwitterIcon },
+  { name: 'Unsplash', src: UnsplashIcon },
+  { name: 'WhatsApp', src: WhatsAppIcon },
+  { name: 'Wordpress', src: WordpressIcon },
+  { name: 'Youtube', src: YoutubeIcon },
+];
+
+export default class ServiceTemplatesProvider {
+  static createServiceTemplate(templateName, name = templateName, icon = '', passwordValue = '') {
+    return {
+      templateName, name, icon, passwordValue,
+    };
+  }
+
+  static getServiceTemplates() {
+    return [
+      this.createServiceTemplate(customService.name, '', customService.src),
+      ...supportingServices.map(ss => (this.createServiceTemplate(ss.name, undefined, ss.src))),
+    ];
+  }
+
+  static getServiceTemplateByName(templateName) {
+    return this.getServiceTemplates().find(template => template.templateName === templateName);
+  }
+}
