477c05864e89eea663da13e9f9788f7f26466b0a
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-03-06 22:29:41 +0100

    Loading flag (loader) based on ajaxStatusReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import routes from '../routes';
 import Header from './common/header/Header';
 import Footer from './common/footer/Footer';
 
-export default () => (
+const App = props => (
   <Fragment>
-    <Header />
+    <Header loading={props.loading} />
     <main>{routes}</main>
     <Footer />
   </Fragment>
 );
+
+function mapStateToProps(state) {
+  return {
+    loading: state.ajaxCallsInProgress > 0,
+  };
+}
+
+App.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
+
+export default withRouter(connect(mapStateToProps)(App));
--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Logo from './logo/Logo';
 import './Header.css';
 
-export default () => {
+const Header = (props) => {
   const applicationName = 'Password backpack';
   const tagline = 'All passwords in one place';
 
   return (
     <header className="application-header">
       <Logo />
+      {props.loading && 'LOADING...'}
       <h1 className="application-heading-text">{applicationName}</h1>
       <h3>{tagline}</h3>
     </header>
   );
 };
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
+
+export default Header;
