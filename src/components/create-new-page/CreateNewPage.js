b14ec7dba1df292e5b8edf98ed66c92f08c2c499
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-03-09 23:53:13 +0100

    no-services state

--- src/components/create-new-page/CreateNewPage.js
+++ src/components/create-new-page/CreateNewPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import AddServiceForm from './add-service-form/AddServiceForm';
+import ListIcon from '../../icons/list.svg';
 import './CreateNewPage.css';
 import Service from './service/Service';
 
@@ -10,7 +11,12 @@ const staticTexts = new Map([
   ['page.go_back', 'Go back'],
   ['alt.service_new', 'Click to add new service'],
   ['desc.service_new', 'Add new service'],
+  ['no_content', 'Append service to see it on the list'],
+  ['no_content.icon.alt', 'No content icon. You must service to see it here.'],
+  ['btn.label.create_ps_file', 'Create password file'],
 ]);
+
+const noContentIconDimension = 80;
 
 class CreateNewPage extends React.Component {
   constructor() {
@@ -18,6 +24,7 @@ class CreateNewPage extends React.Component {
     this.goToDashboard = this.goToDashboard.bind(this);
     this.appendService = this.appendService.bind(this);
     this.dropService = this.dropService.bind(this);
+    this.makePasswordFile = this.makePasswordFile.bind(this);
 
     this.state = {
       services: [],
@@ -38,29 +45,61 @@ class CreateNewPage extends React.Component {
     });
   }
 
+  isServiceListEmpty() {
+    return !this.state.services.length;
+  }
+
+  makePasswordFile() {
+    console.log('Creating password file: ', JSON.stringify(this.state.services));
+  }
+
   render() {
     return (
-      <section>
+      <section className="create-new-page">
         <header>
           <button onClick={this.goToDashboard}>{staticTexts.get('page.go_back')}</button>
           <h1 className="create-new-page-heading-text">{staticTexts.get('page.create_new.header')}</h1>
         </header>
         <AddServiceForm onFormSubmit={this.appendService} />
+
         <hr />
-        <section className="service-list">
-          {this.state.services.map(element => (
-            <Service
-              key={element.service.id}
-              id={element.service.id}
-              icon={element.service.icon}
-              name={element.service.name}
-              templateName={element.service.templateName}
-              passwordValue={element.service.passwordValue}
-              passwordTypeValue={element.passwordType.value}
-              onDeleteClick={this.dropService}
+        {
+          this.isServiceListEmpty() &&
+          <div className="no-services-container">
+            <img
+              className="no-services-icon"
+              alt={staticTexts.get('no_content.icon.alt')}
+              src={ListIcon}
+              width={noContentIconDimension}
+              height={noContentIconDimension}
             />
+            <h3>{staticTexts.get('no_content')}</h3>
+          </div>
+        }
+
+        <section className="service-list">
+          {
+            this.state.services.map(element => (
+              <Service
+                key={element.service.id}
+                id={element.service.id}
+                icon={element.service.icon}
+                name={element.service.name}
+                templateName={element.service.templateName}
+                passwordValue={element.service.passwordValue}
+                passwordTypeValue={element.passwordType.value}
+                onDeleteClick={this.dropService}
+              />
           ))}
         </section>
+        <hr />
+
+        <button
+          onClick={this.makePasswordFile}
+          disabled={this.isServiceListEmpty()}
+        >
+          {staticTexts.get('btn.label.create_ps_file')}
+        </button>
       </section>
     );
   }
