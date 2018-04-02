d688860f8a70499852863c165c544e58262f5f19
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-04-02 23:26:00 +0200

    Removed default README content, migration to electron-builder, unused dependencies removal

---
+++ prebuild.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+function copyFile(source, target) {
+  const rd = fs.createReadStream(source);
+  const wr = fs.createWriteStream(target);
+
+  return new Promise(((resolve, reject) => {
+    rd.on('error', reject);
+    wr.on('error', reject);
+    wr.on('finish', resolve);
+    rd.pipe(wr);
+  })).catch((error) => {
+    rd.destroy();
+    wr.end();
+    throw error;
+  });
+}
+
+copyFile(
+  './icon.icns',
+  './build/icon.icns',
+);
