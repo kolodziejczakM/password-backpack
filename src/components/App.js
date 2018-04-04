1ec3beda6d4672bf3eadbfd8a1e50aa5ec7296d7
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-04-04 20:19:41 +0200

    Authors modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import routes from '../routes';
 import Header from './common/header/Header';
-import Footer from './common/footer/Footer';
 
 // Thanks to this flag user don't see "being online alert" every time he navigates between pages.
 window.allowUnsafeMode = false;
@@ -10,6 +9,5 @@ export default () => (
   <Fragment>
     <Header />
     <main>{routes}</main>
-    <Footer />
   </Fragment>
 );
--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import swal from 'sweetalert';
 import Logo from './logo/Logo';
 import NetworkStatusBar from '../network-status-bar/NetworkStatusBar';
 import CloseButton from '../close-button/CloseButton';
@@ -8,9 +9,30 @@ const staticTexts = new Map([
   ['app.name', 'Password backpack'],
   ['app.tagline', 'All passwords in one place'],
   ['app.authors', 'Authors'],
+  ['authors.leading', 'Programmer: Marcin Kołodziejczak'],
+  ['authors.title', 'Authors'],
+  ['authors.icon', 'Icon authors (www.flaticon.com):'],
 ]);
 
-export default () => (
+const showAuthorsModal = () => {
+  const iconAuthors = [
+    'Pixel Buddha', 'Freepik', 'Dave Gandy', 'Pixel perfect', 'UIUXER', 'Smashicons', 'Eleonor Wang',
+  ]
+    .map(author => `${author}\n`)
+    .join('');
+
+  swal({
+    title: staticTexts.get('authors.title'),
+    text: `
+      ${staticTexts.get('authors.leading')}
+
+      ${staticTexts.get('authors.icon')}
+        ${iconAuthors}
+    `,
+  });
+};
+
+const Header = () => (
   <header className="application-header">
     <div className="application-header__close-app-btn">
       <CloseButton />
@@ -23,8 +45,15 @@ export default () => (
     <div className="application-header__upper-left-corner">
       <NetworkStatusBar />
       <div className="show-authors-btn-container">
-        <button className="show-authors-btn">{staticTexts.get('app.authors')}</button>
+        <button
+          onClick={showAuthorsModal}
+          className="show-authors-btn"
+        >
+          {staticTexts.get('app.authors')}
+        </button>
       </div>
     </div>
   </header>
 );
+
+export default Header;
